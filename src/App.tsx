db39e253fcd534a81d5fbc93234115949c476002
Steve Hughes <steve.hughes@creative-technologies.co.uk>
2020-05-02 18:17:09 +0100

    Added more truths

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const truths = ["he isn't", "he has never been", "he will never be"];
+
 function App() {
+  const [stage, setStage] = useState<number>(0);
+  const onClick = () => {
+    setStage(stage + 1);
+  }
+
+  console.log(`stage: ${stage}`);
+  const truth = truths[stage % 3];
   return (
-    <div className="App">
+    <div className="App" onClick={onClick}>
       <main className="get-out-of-here">
-        He Isn't
+        {truth}
       </main>
     </div>
   );
